6b1fc6aec809f5ad84394fe07b3a2445f27b7e53
Alektor <panagiotispp@gmail.com>
2016-07-26 21:11:47 +0200

    server2.js example reads from index.html

---
+++ server2.js
@@ -0,0 +1,22 @@
+var http = require("http")
+var fs = require('fs')
+
+http.createServer( function (request, response){
+
+    // console.log(request)
+    console.log("There is a request!")
+
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    var html = fs.readFileSync(__dirname + "/index.html")
+    response.end(html)
+
+
+
+}).listen(1337, '127.0.0.1')
+
+// I hardcode the response
+// the request will be sent by the browser!
+
+// if I write localhost in my pc it is translated as 127.0.0.1
+
+// $ node server.js <--- to run the server
