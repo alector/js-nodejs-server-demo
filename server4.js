8714ce43240614b27c902cbc36de82e312a9f34d
Alektor <panagiotispp@gmail.com>
2016-07-26 21:36:30 +0200

    server4 creates an api serializing an object into json

---
+++ server4.js
@@ -0,0 +1,27 @@
+var http = require("http")
+var fs = require('fs')
+
+
+// THIS IS A WEB API RETURNING JSON
+http.createServer( function (req_stream, resp_stream){
+
+    // console.log(request)
+    console.log("There is a request!")
+
+    var data = {
+        name: "Peter", 
+        age: 30
+    }
+
+
+    resp_stream.end(JSON.stringify(data))
+
+
+}).listen(1337, '127.0.0.1')
+
+// I hardcode the response
+// the request will be sent by the browser!
+
+// if I write localhost in my pc it is translated as 127.0.0.1
+
+// $ node server.js <--- to run the server
