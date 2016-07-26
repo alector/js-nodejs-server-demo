01464f8eda6d616c03449230427b15476dcc5ee6
Alektor <panagiotispp@gmail.com>
2016-07-26 20:52:19 +0200

    change name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require("http")
 http.createServer( function (request, response){
 
     response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end("Hello world!\n by Panos\n")
+    response.end("Hello world!\n by Alector\n")
 
 }).listen(1337, '127.0.0.1')
 
